ac7f4271ea55743b7e7e4c756f865f29b94e1bc4
nabetama <mao.nabeta@gmail.com>
2020-03-05 01:36:09 +0900

    tidy

--- src/test/java/com/nabetama/factorymethod/idcard/IDCardTest.java
+++ src/test/java/com/nabetama/factorymethod/idcard/IDCardTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class IDCardTest {
 
   @Test
-  void interfaceTest() {
+  void testInterface() {
     IDCard idCard = new IDCard("nabetama");
     assertEquals("nabetama", idCard.getOwner());
     assertEquals("It will be making card that owned by nabetama", idCard.use());
