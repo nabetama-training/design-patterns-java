9fb7763cdf10850facbad6ebb305a40547a24632
nabetama <mao.nabeta@gmail.com>
2020-03-09 23:54:46 +0900

    unittest

---
+++ src/test/java/com/nabetama/abstractfactory/AbstractFactoryTest.java
@@ -0,0 +1,11 @@
+package com.nabetama.abstractfactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AbstractFactoryTest {
+
+  AbstractFactory abstractFactory1 = new Factory1Impl();
+  AbstractFactory abstractFactory2 = new Factory2Impl();
+
+  // TODO: write test cases
+}
