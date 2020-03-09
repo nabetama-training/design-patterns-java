f046fe1c956db5498cb0632f2728b940780b974a
nabetama <mao.nabeta@gmail.com>
2020-03-10 00:10:37 +0900

    add unittest

--- src/test/java/com/nabetama/abstractfactory/AbstractFactoryTest.java
+++ src/test/java/com/nabetama/abstractfactory/AbstractFactoryTest.java
@@ -2,10 +2,30 @@ package com.nabetama.abstractfactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class AbstractFactoryTest {
 
   AbstractFactory abstractFactory1 = new Factory1Impl();
   AbstractFactory abstractFactory2 = new Factory2Impl();
 
-  // TODO: write test cases
+  @Test
+  void testFactoryA() {
+    assertEquals(
+        abstractFactory1.createProductA().getClass().getName(),
+        "com.nabetama.abstractfactory.ProductA1");
+    assertEquals(
+        abstractFactory2.createProductA().getClass().getName(),
+        "com.nabetama.abstractfactory.ProductA2");
+  }
+
+  @Test
+  void testFactoryB() {
+    assertEquals(
+        abstractFactory1.createProductB().getClass().getName(),
+        "com.nabetama.abstractfactory.ProductB1");
+    assertEquals(
+        abstractFactory2.createProductB().getClass().getName(),
+        "com.nabetama.abstractfactory.ProductB2");
+  }
 }
