199327298c0777b2b75e9f5373a82a614b199dde
nabetama <mao.nabeta@gmail.com>
2020-03-22 18:40:53 +0900

    Fixup visitor interface

--- src/main/java/com/nabetama/visitor/Visitor.java
+++ src/main/java/com/nabetama/visitor/Visitor.java
@@ -1,14 +1,12 @@
 package com.nabetama.visitor;
 
 
-import com.nabetama.composite.files.Directory;
-import com.nabetama.composite.files.File;
-
 /**
  * Declare operation
  */
 public interface Visitor {
 
   void visit(File file);
+
   void visit(Directory directory);
 }
